f8bcec9105624d3175fd7fe239c48f4731f1b88f
João Vitor Denari dos Santos <98748740+jdenari@users.noreply.github.com>
2023-02-21 17:37:47 -0300

    sql-update

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const mysql = require("mysql");
 
 // routes
 const authRouter = require("./routes/authRoutes");
@@ -35,6 +36,40 @@ app.get("/", (req, res) => {
 mongoose.connect(
     `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.06ovsjg.mongodb.net/users?retryWrites=true&w=majority`,
 );
+
+const db = mysql.createConnection({
+    host: "sql813.main-hosting.eu",
+    user: "u880384778_joaodenari",
+    password: "Hdga29*ajpTR",
+    database: "u880384778_address_list",
+});
+
+db.connect((err) => {
+    if (err) {
+      console.error("error connecting to MySQL server: " + err.stack);
+      return;
+    }
+    console.log("connected to MySQL server as id " + db.threadId);
+});
+
+app.use((req, res, next) => {
+    console.log('oiii')
+    req.db = db;
+    console.log('oi')
+    next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use("/api/auth", authRouter)
+app.use("/api/user", userRouter)
+app.use("/api/address", addressesExampleRouter)
+
+app.get("/", (req, res) => {
+    res.json({ message: "Rota teste"})
+})
 
 app.listen(port, () => {
     console.log(`Our variable ${process.env.MY_VARIABLE}`)
